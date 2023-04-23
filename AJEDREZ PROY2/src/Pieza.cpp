6508bad299eecbd9b87676e90f8098f175c67127
joseenriqueespinoza99 <79969798+joseenriqueespinoza99@users.noreply.github.com>
2023-04-23 23:21:05 +0200

    Definición de las funciones de la clase Pieza

--- AJEDREZ PROY2/src/Pieza.cpp
+++ AJEDREZ PROY2/src/Pieza.cpp
@@ -1,2 +1,68 @@
 #include "Pieza.h"
+Pieza pieza;
+void Pieza::dibujar(const char foto[], int x1, int y1)
+{
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, ETSIDI::getTexture(foto).id);
+    glDisable(GL_LIGHTING);
+    glBegin(GL_POLYGON);
+    glColor3f(1, 1, 1);
+    glTexCoord2d(0, 0);    glVertex2f(x1, y1);
+    glTexCoord2d(1, 0);    glVertex2f(x1+1, y1);
+    glTexCoord2d(1, 1);    glVertex2f(x1+1, y1-1);
+    glTexCoord2d(0, 1);    glVertex2f(x1, y1-1);
+    glEnd();
+    glEnable(GL_LIGHTING);
+    glDisable(GL_TEXTURE_2D);
+}
 
+void Pieza::pintar(const char foto1[], const char foto2[], const char foto3[], const char foto4[], int x1,int y1)//funcion que cambia el fondo de las piezas dependiendo de las coordenadas
+{
+    for (int j = 0; j <9; j++) {//bucle que va de 0 a 9 y suma 1
+        for (int i = 0; i < 9; i++) {//bucle mas rapido que va de 0 a 9 y suma 1
+            if (x1 == j && y1 == i) { //si i y j son iguales a x e y
+                if (j % 2 == 1) {//Bucle para las coordenadas y, 
+                    if (i % 2 == 1) {//Bucle para las coordenadas x
+                        if (color==true)
+                            pieza.dibujar(foto4, j,i);
+                        else
+                            pieza.dibujar(foto2, j,i);
+                    }
+                    else
+                    {
+                        if (color == true)
+                            pieza.dibujar(foto1, j, i);
+                        else
+                            pieza.dibujar(foto3, j, i);
+
+
+                    }
+                }
+                else
+                {
+                    if (i % 2 == 1) {//Bucle para las coordenadas x
+                        if (color == true)
+                            pieza.dibujar(foto2, j, i);
+                        else
+                            pieza.dibujar(foto4, j, i);
+                    }
+                    else
+                    {
+                        if (color == true)
+                            pieza.dibujar(foto3, j, i);
+                        else
+                            pieza.dibujar(foto1, j, i);
+                    }
+                }
+            }
+        }
+    }
+    
+}
+
+
+
+void Pieza::setColor(bool c)
+{
+    color = c;
+}
