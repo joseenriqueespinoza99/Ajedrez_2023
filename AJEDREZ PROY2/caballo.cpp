dbae4a216a635534138b590c5bc0b999b239cf18
Alvallcue <alvaro.allende.cueva@alumnos.upm.es>
2023-04-23 17:09:46 +0200

    cambios en el movimiento
    mejoras en el movimiento del caballo

--- AJEDREZ PROY2/caballo.cpp
+++ AJEDREZ PROY2/caballo.cpp
@@ -7,32 +7,47 @@ using namespace std;
 
 void caballo::mueveCab() {
 	caballo posAct[2]{ fila, columna };
-	//caballo se mueve en L, avanza 2 y luego 1, nunca nada vertical
-	int k; //incrementos de posicion
-	
+	//caballo se mueve en L, avanza 2 y luego 1
+	//incrementos de posicion
+	int k;
 	switch (k) {
 	case 1:
-		if (movH = 'd') {
-			if (movV = 'r') {
-				caballo posSig[2]{ posAct[fila + 1, columna + 2] };
-			}
-		else if(movV = 'a') caballo posSig[2]{ posAct[fila + 2, columna + 1] };
+		//si va hacia la derecha y solo cambia 1 fila y 2 columnas
+		if (movH == "rd") {
+			if (movV = 'a') caballo posSig[2]{ posAct[fila + 1, columna + 2] };	
+			else if(movV = 'd') caballo posSig[2]{ posAct[fila - 1, columna + 2] };
 			else cout << "Movimiento erroneo";
 		}
 		else cout << "Movimiento erroneo";
 		break;
 
 	case 2:
-		if (movH = 'i') {
-			if (movV = 'r') {
-				caballo posSig[2]{ posAct[fila + 2, columna - 1] };
-			}
-			else if (movV = 'a') caballo posSig[2]{ posAct[fila + 1, columna - 2] };
+		//si va hacia la izquierda  y solo cambia 1 fila y 2 columnas
+		if (movH == "ri") {
+			if (movV = 'a') caballo posSig[2]{ posAct[fila - 1, columna - 2] };		
+			else if (movV = 'd') caballo posSig[2]{ posAct[fila + 1, columna - 2] };
 			else cout << "Movimiento erroneo";
 		}
 		else cout << "Movimiento erroneo";
 		break;
+	case 3:
+		//derecha y solo cambia 2 filas 1 columna
+		if (movH == "nd") {
+			if (movV = 'a') caballo posSig[2]{ posAct[fila - 2, columna + 1] };
+			else if (movV = 'd') caballo posSig[2]{ posAct[fila + 2, columna + 1] };
+			else cout << "Movimiento erroneo";
+		}
+		else cout << "Movimiento erroneo";
+		break;
+	case 4:
+		//izquierda, solo cambia 2 filas y 1 columna
+		if (movH == "ni") {
+			if (movV = 'a') caballo posSig[2]{ posAct[fila + 2, columna -1] };
+			else if (movV = 'd') caballo posSig[2]{ posAct[fila - 2, columna - 1] };
+			else cout << "Movimiento erroneo";
+		}
 	}
+	
 
 
 	}
--- AJEDREZ PROY2/caballo.h
+++ AJEDREZ PROY2/caballo.h
@@ -9,8 +9,9 @@ public:
 	unsigned int columna;
 	int posAct[2];
 	int posSig[2];
+	int k;
 	char movV;
-	char movH;
+	char movH[2];
 	void mueveCab();
 	
 };
