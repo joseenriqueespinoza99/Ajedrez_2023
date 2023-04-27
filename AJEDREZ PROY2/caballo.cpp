cb3993a38fd5daab780e82fcee94ee7764af3934
Alvallcue <alvaro.allende.cueva@alumnos.upm.es>
2023-04-27 11:28:46 +0200

    solución de errores
    Arreglo de errores que impedían la compilación del código

--- AJEDREZ PROY2/caballo.cpp
+++ AJEDREZ PROY2/caballo.cpp
@@ -5,76 +5,60 @@
 
 using namespace std;
 
-
+caballo Caballo;
 
 caballo::~caballo() {
 
 }
 
 void caballo::mueveCab(char p[2], char z) {
-
+	
 	if (p == "rd") {
-
+		columna = columna + 2;
+		if (z == 'a') {
+			fila++;		
+		}
+		else if (z == 'd') {
+			fila--;
+		}
 	}
 	else if (p == "ri") {
-
+		columna = columna - 2;
+		if (z == 'a') {
+			fila++;
+		}
+		else if (z == 'd') {
+			fila--;
+		}
 	}
 	else if (p == "nd") {
-
+		columna++;
+		if (z == 'a') {
+			fila = fila + 2;
+		}
+		else if (z == 'd') {
+			fila = fila - 2;
+		}
 	}
 	else if (p == "ni") {
-
+		columna--;
+		if (z == 'a') {
+			fila = fila + 2;
+		}
+		else if (z == 'd') {
+			fila = fila - 2;
+		}
+		setx(fila);
+		sety(columna);
+		glutPostRedisplay;
 	}
-	/*
-	caballo posAct[2]{ fila, columna };
-	//caballo se mueve en L, avanza 2 y luego 1
-	//incrementos de posicion
-	char k;
-	switch (k) {
-	case 1:
-		//si va hacia la derecha y solo cambia 1 fila y 2 columnas
-		if (movH == "rd") {
-			if (movV = 'a')  posSig[2]{ posAct[fila + 1, columna + 2] };
-			else if (movV = 'd') caballo posSig[2]{ posAct[fila - 1, columna + 2] };
-			else cout << "Movimiento erroneo";
-		}
-		else cout << "Movimiento erroneo";
-		break;
-	case 2:
-		//si va hacia la izquierda  y solo cambia 1 fila y 2 columnas
-		if (movH == "ri") {
-			if (movV = 'a') caballo posSig[2]{ posAct[fila - 1, columna - 2] };
-			else if (movV = 'd') caballo posSig[2]{ posAct[fila + 1, columna - 2] };
-			else cout << "Movimiento erroneo";
-		}
-		else cout << "Movimiento erroneo";
-		break;
-	case 3:
-		//derecha y solo cambia 2 filas 1 columna
-		if (movH == "nd") {
-			if (movV = 'a') caballo posSig[2]{ posAct[fila - 2, columna + 1] };
-			else if (movV = 'd') caballo posSig[2]{ posAct[fila + 2, columna + 1] };
-			else cout << "Movimiento erroneo";
-		}
-		else cout << "Movimiento erroneo";
-		break;
-	case 4:
-		//izquierda, solo cambia 2 filas y 1 columna
-		if (movH == "ni") {
-			if (movV = 'a') caballo posSig[2]{ posAct[fila + 2, columna - 1] };
-			else if (movV = 'd') caballo posSig[2]{ posAct[fila - 2, columna - 1] };
-			else cout << "Movimiento erroneo";
-		}
-		else cout << "Movimiento erroneo";
-		*/
+	
 	}
-
+/*
 		bool caballo::comprobacion(int x, int y) {
 			//si hay 2 casillas de diferencia entre columna inicial y final y 1 entre filas sera correcto
 			//x =  posSig[0];
 			//y = posSig[1];
-
-			caballo posSig[2]{ x,y };
 
 			if ((x - fila) == 1 && ((y - columna) == 2)) {
 				return true;
@@ -85,6 +69,18 @@ void caballo::mueveCab(char p[2], char z) {
 			}
 			else return false;
 		}
+
 	
+		void caballo::dibujaCab() {
+			Caballo.setColor(true);
+			if (Caballo.getColor() == true) {
+				Caballo.pintar("imagenes/torrenegra.png", "imagenes/torreblanca.png", "imagenes/torrenblanca.png", "imagenes/torrennegra.png", getx(), gety() + 1);
+				casilla_torre = true;
+			}
+			else {
+				torre.pintar("imagenes/torreblanca.png", "imagenes/torrenegra.png", "imagenes/torrennegra.png", "imagenes/torrenblanca.png", getx(), gety() + 1);
+				casilla_torre = true;
+			}
+			*/
 
 
--- AJEDREZ PROY2/caballo.h
+++ AJEDREZ PROY2/caballo.h
@@ -11,15 +11,13 @@ public:
 	bool Color;
 	int fila;
 	int columna;
-	int posAct[2];
-	int posSig[2];
+	
 public:
 	//parametros para saber donde esta el caballo
-	caballo(int fila, int columna, bool Color);
 	virtual ~caballo();
-	char movV;
-	char movH[2];
 	void mueveCab(char p[2], char z);
+	void setx(int xi) { fila = xi; }
+	void sety(int yi) { columna = yi; }
 	bool comprobacion(int x, int y);
 	void dibujaCab();
 };
