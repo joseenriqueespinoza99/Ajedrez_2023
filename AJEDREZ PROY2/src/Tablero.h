3874320d496c50332f4d523a040506aa09bf8121
joseenriqueespinoza99 <79969798+joseenriqueespinoza99@users.noreply.github.com>
2023-05-01 12:18:16 +0200

    Update Tablero.h

--- AJEDREZ PROY2/src/Tablero.h
+++ AJEDREZ PROY2/src/Tablero.h
@@ -1,8 +1,6 @@
 #pragma once
-#define MAX_PIEZAS 32
 #include "ListaPiezas.h"
 #include "freeglut.h"
-#include "ETSIDI.h"
 struct posicion{
 	int posx;
 	int posy;
@@ -12,13 +10,9 @@ class Tablero
 private:
 	posicion origen;//sirve para determinar la posición origen
 	posicion destino;//sirve para determinar la posicion de destino
-	bool turno;
-	bool final;
 	ListaPiezas listapiezas;
 	public:
-	Tablero();
-	Pieza* casillaOcupaBlanca[MAX_PIEZAS / 2];
-	Pieza* casillaOcupaNegra[MAX_PIEZAS / 2];
+	//	Tablero() {};
 		void setX_origen(int x) { origen.posx = x; }
 		void setY_origen(int y) { origen.posy = y; }
 		void setX_destino(int x) { destino.posx = x; }
@@ -30,9 +24,5 @@ private:
 		void dibuja();
 		void inicializa();
 		void mueve(unsigned char key);
-		bool EspacioBlancas(Pieza* c);
-		bool EspacioNegras(Pieza* c);
-		bool EspacioCasilla(Pieza* p);
-		void cambiarTurno();
 //Definición del destructor
 	};
