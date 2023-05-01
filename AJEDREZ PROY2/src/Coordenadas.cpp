87cc4802ae57129f78af2829f229169c39251bcb
Alvallcue <alvaro.allende.cueva@alumnos.upm.es>
2023-05-01 19:21:22 +0200

    Funciones del movimiento

---
+++ AJEDREZ PROY2/src/Coordenadas.cpp
@@ -0,0 +1,14 @@
+#include "Coordenadas.h"
+
+Coordenadas::Coordenadas(int xAct, int yAct) {
+	x = xAct;
+	y = yAct;
+}
+
+Coordenadas::~Coordenadas() {
+	//destruir coordenadas siguiente
+}
+
+int Coordenadas::cambioPos(int x1[2], int x2[2])  {
+
+}
--- AJEDREZ PROY2/src/Coordenadas.h
+++ AJEDREZ PROY2/src/Coordenadas.h
@@ -1,8 +1,19 @@
 #pragma once
 class Coordenadas
 {
+protected:
+	int x;
+	int y;
+	int posAct[2];
+	int posSig[2];
 
-
-
+public:
+	Coordenadas(int xAct, int yAct);
+	virtual ~Coordenadas();
+	int setX() { return x; }
+	int setY() { return y; }
+	int getX(int xi) { this->x = xi; return x; }
+	int getY(int yi) { this->y = yi; return y; }
+	int cambioPos(int x1[2], int x2[2]) { this->posAct[2] = x1[2]; this->posSig[2] = x2[2]; }
 };
 
