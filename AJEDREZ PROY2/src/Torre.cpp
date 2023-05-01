3f0e5419366c85f7e4f3f4ff9b876fec04a32841
joseenriqueespinoza99 <79969798+joseenriqueespinoza99@users.noreply.github.com>
2023-05-01 11:35:48 +0200

    Actualización Torre.cpp

--- AJEDREZ PROY2/src/Torre.cpp
+++ AJEDREZ PROY2/src/Torre.cpp
@@ -1,9 +1,3 @@
-#include "Torre.h"
-
-Torre::Torre()
-{
-
-}
 Torre::Torre(int x, int y, bool color) {
 	this->posx = x;
 	this->posy = y;
@@ -36,13 +30,156 @@ void Torre::mueve(unsigned char key)
 }
 
 void Torre::dibuja() {
-	setColor(true);
-			if (getColor() == true) {
+			if (color == true) {
 				pintar("imagenes/torrenegra.png", "imagenes/torreblanca.png", "imagenes/torrenblanca.png", "imagenes/torrennegra.png", getX(), getY()+1);
-				casilla_torre = true;
+				//casilla_torre = true;
 			}
 			else {
 				pintar("imagenes/torreblanca.png", "imagenes/torrenegra.png", "imagenes/torrennegra.png", "imagenes/torrenblanca.png", getX(), getY()+1);
-				casilla_torre = true;
+				//casilla_torre = true;
 			}
+}
+
+void Torre::movimientos() {
+	int var1 = 0, var2 = 0, var3 = 0, var4 = 0; 
+	bool encontrado = false;
+	Pieza* coordenadas_de_consulta;
+	
+	for (int i = getX() + 1; i <= 8; i++) {
+		coordenadas_de_consulta->setX(i);
+		coordenadas_de_consulta->setX(getY());
+
+		
+		if (getColor() && t->EspacioBlancas(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var1 = i - getX() - 1;
+		}
+		else if (!getColor() && t->EspacioNegras(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var1 = i - getX() - 1;
+		}
+
+		
+		if (!getColor() && t->EspacioBlancas(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var1 = i - getX();
+		}
+		else if (getColor() && t->EspacioNegras(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var1 = i - getX();
+		}
+	}
+	if (!encontrado) 
+		var1 = 8 - getX();
+
+	encontrado = false; 
+	for (int i = getX() - 1; i >= 1; i--) {
+		coordenadas_de_consulta->setX(i);
+		coordenadas_de_consulta->setY(getY());
+
+		
+		if (getColor() && t->EspacioBlancas(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var2 = getX() - i - 1;
+		}
+		else if (!getColor() && t->EspacioNegras(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var2 = getX() - i - 1;
+		}
+
+		
+		if (!getColor() && t->EspacioBlancas(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var2 = getX() - i;
+		}
+		else if (getColor() && t->EspacioNegras(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var2 = i - getX() - i;
+		}
+	}
+	if (!encontrado)
+		var2 = getX() - 1;
+
+
+	encontrado = false;
+	for (int i = getY() + 1; i <= 8; i++) {
+		coordenadas_de_consulta->setX(getX());
+		coordenadas_de_consulta->setY(i);
+
+		
+		if (getColor() && t->EspacioBlancas(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var3 = i - getY() - 1;
+		}
+		else if (!getColor() && t->EspacioNegras(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var3 = i - getY() - 1;
+		}
+
+		
+		if (!getColor() && t->EspacioBlancas(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var3 = i - getY();
+		}
+		else if (getColor() && t->EspacioNegras(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var3 = i - getY();
+		}
+	}
+	if (!encontrado) 
+		var3 = 8 - getY();
+
+
+	encontrado = false; 
+	for (int i = getY() - 1; i >= 1; i--) {
+		coordenadas_de_consulta->setX(getX());
+		coordenadas_de_consulta->setY(i);
+
+		
+		if (getColor() && t->EspacioBlancas(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var4 = getY() - i - 1;
+		}
+		else if (!getColor() && t->EspacioNegras(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var4 = getY() - i - 1;
+		}
+
+		
+		if (!getColor() && t->EspacioBlancas(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var4 = getY() - i;
+		}
+		else if (getColor() && t->EspacioNegras(coordenadas_de_consulta) && !encontrado) {
+			encontrado = true;
+			var4 = i - getY() - i;
+		}
+	}
+	if (!encontrado)
+		var4 = getY() - 1;
+
+
+	for (int i = 0; i < var1; i++) 
+	{
+		muv[i]->setX(getX() + i + 1);
+		muv[i]->setX(getY());
+	}
+	for (int i = 0; i < var2; i++) 
+	{
+		muv[i + var1]->setX(getX() - i - 1);
+		muv[i + var1]->setX(getY());
+	}
+	for (int i = 0; i < var3; i++) 
+	{
+		muv[i + var1 + var2]->setX(getX());
+		muv[i + var1 + var2]->setX(getY() + i + 1);
+	}
+	for (int i = 0; i < var4; i++) 
+	{
+		muv[i + var1 + var2 + var3]->setX(getX());
+		muv[i + var1 + var2 + var3]->setX(getY() - i - 1);
+	}
+
+	
+
 }
