a235f082e4b64d011a54beada6c79e8ea5c84d97
laura-alvrz <119267937+laura-alvrz@users.noreply.github.com>
2023-05-05 15:35:35 +0200

    Implementacion de rey
    Dibuja y movimientos

--- AJEDREZ PROY2/src/Rey.h
+++ AJEDREZ PROY2/src/Rey.h
@@ -1,7 +1,17 @@
 #pragma once
-class Rey
-{
-};
+#include "Pieza.h"
+#include "Tablero.h"
+#include "freeglut.h"
 
+class Rey :public Pieza {
+public:
+	Rey(int x, int y, bool color);
+	Rey() {};
+	void dibuja() override;
+	void mueve(unsigned char key) override;
+	bool esmovimientoValido(int x_destino, int y_destino) override;
+	void mover(int x, int y)override;
+	virtual TipoPieza getClass() const override { return TipoPieza::REY; }
+};
 
 
