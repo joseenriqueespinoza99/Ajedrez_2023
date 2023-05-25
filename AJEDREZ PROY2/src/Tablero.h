69c6f2bffd8dc209781239dcccde48cb3ed25f8a
solomiya.havryshko <61050036+solomiyahav@users.noreply.github.com>
2023-05-25 20:53:58 +0200

    función elimina jaque

--- AJEDREZ PROY2/src/Tablero.h
+++ AJEDREZ PROY2/src/Tablero.h
@@ -50,6 +50,6 @@ public:
 	Pieza* coronacion(int x, int y);
 	void comer_al_paso(int origen_x, int origen_y, int destino_x, int destino_y, bool color);
 	bool casillaEnJaque(bool colorRey, int x, int y);
-
+	bool quitarJaque();
 };
 
